fba3f89662df1970b9b80c2e0a3bb882c6b6a4da
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-06-25 14:00:16 +0300

    Python and C solutions uploaded

---
+++ C/8kyu/hellowWorld.c
@@ -0,0 +1,9 @@
+/*
+ * Kata's URL - https://www.codewars.com/kata/523b4ff7adca849afe000035/train/c
+ * Difficulty - 8kyu
+ * Submitter - karastoyanov
+ */
+
+char *greet(void){
+  return "hello world!";
+}
---
+++ Python/8kyu/convert_boolen_to_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Kata's URL - https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+Difficulty - 8kyu
+Submitter - karastoyanov
+'''
+
+def bool_to_word(boolean):
+    return "Yes" if boolean == True else "No"
+
+
+print(bool_to_word(True))
+print(bool_to_word(False))
