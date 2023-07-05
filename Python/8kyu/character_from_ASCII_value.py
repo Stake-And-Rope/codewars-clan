6d8bbcfe301ba3a5469d7acd18ac0f6aa2c01c6d
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2023-07-05 22:35:28 +0300

    Group Session Solution solutions on 05.07  - alex  - raya  - dancho

---
+++ Python/8kyu/character_from_ASCII_value.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''
+Kata's URL - https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+Difficulty - 8kyu
+Submitter - karastoyanov, raya, dancho
+'''
+
+
+def get_char(c):
+    return chr(c)
---
+++ Python/8kyu/reversed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+Kata's URL - https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/python
+Difficulty - 8kyu
+Submitter - karastoyanov, raya, dancho
+'''
+
+def reverse_list(l):
+    return l[::-1]
+
+
+print(reverse_list([1,2,3,4]))
